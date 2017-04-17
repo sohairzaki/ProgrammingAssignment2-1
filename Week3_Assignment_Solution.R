a3141b33b407277959d0635406556cf89c6fac1d
Sohair Zaki <sohairzaki@yahoo.com>
2017-04-17 19:24:31 -0400

    Add files via upload

---
+++ Week3_Assignment_Solution.R
@@ -0,0 +1,44 @@
+
+
+setwd("/DataScienceCourse_Coursera/RProgramming_Course_Course2/RProgramming_Course2_Week3")
+makeCacheMatrix <- function(x = matrix()) {
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) m <<- inverse
+    getinverse <- function() m
+    list(set = set,
+         get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
+}
+
+
+    
+    cacheSolve <- function(x, ...) {
+        m <- x$getinverse()
+        if (!is.null(m)) {
+            message("getting cached data")
+            return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
+    }
+    
+    #unit testing
+    A <- matrix(c(3,4,5,6),2,2)
+    B <- makeCacheMatrix(A)
+    cacheSolve(B)
+    
+    #unit testing 2
+    A<- matrix (c(4,2,7,6),2,2)
+    B<- makeCacheMatrix (A)
+    cacheSolve (B)
+    
+    
+    
